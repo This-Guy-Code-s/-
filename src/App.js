905ecf5ba7d2d789b6c5a2ceea9d90fcd8bcc6cf
this-guy-code-s <codingviewdmv@gmail.com>
2020-04-06 17:35:17 -0400

    new...pending ios comp. ver

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import {Route} from 'react-router-dom'
 import logo from './img/tgc1.png'
 import me from './img/me.png'
 import bg from './img/bg6.jpg'
+import mbg from './img/bg6mobile.jpg'
 
 import Header from './Header'
 import Home from './comps/'
@@ -16,7 +17,7 @@ import Projects from './comps/projects/'
 const store = createStore(reducer,applyMiddleware(thunk))
 
 
-
+   
 
 class App extends Component {
   
@@ -26,23 +27,45 @@ class App extends Component {
 
 
     componentDidMount(){
-      document.querySelector('.App')
-      .style=`
+     
+
+
+        if(window.innerWidth===500){
+
+             document.querySelector('.App')
+            .style=`
       background-image:url(${bg});
       background-position:center;
       -moz-background-size:cover;
       -webkit-background-size:cover;
       background-size:cover;
-      `
+    
 
+     `
+        }else{
 
+       document.querySelector('.App')
+      .style=`
+      background-image:url(${mbg});
+      background-position:center;
+      -moz-background-size:cover;
+      -webkit-background-size:cover;
+      background-size:cover;
+    
+
+     `
+  
+        }
+    
+
+ 
     }
 
 
 
-
-
   render() {
+
+
 
     return (
       <Provider store={store}>
