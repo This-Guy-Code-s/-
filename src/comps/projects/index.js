7734742917ff563d6feb067f36d2165417cd0a0c
tgc <codingviewdmv@gmail.com>
2020-05-23 17:56:42 -0400

    attempt to fix flex bug on mobile

--- src/comps/projects/index.js
+++ src/comps/projects/index.js
@@ -10,7 +10,7 @@ import styled from 'styled-components'
 
 const Row = styled.div`flex:1;display:flex;flex-direction:column;justify-content:space-around;align-items:center;grid-gap:50px;`;
 
-const cardStylez = {flex:"1",width:'100%',height:'100%',backgroundColor:'rgba(0,0,0,.5)',WebkitTextFillColor:'azure',fontSize:window.innerWidth<=700?'1.15rem':'1.4rem',fontWeight:'bolder',border:'double #4df7ff',boxShadow:'0 0 20px #5539f3'}
+const cardStylez = {width:'100%',height:'auto',backgroundColor:'rgba(0,0,0,.5)',WebkitTextFillColor:'azure',fontSize:window.innerWidth<=700?'1.15rem':'1.4rem',fontWeight:'bolder',border:'double #4df7ff',boxShadow:'0 0 20px #5539f3'}
 
 
 
@@ -47,7 +47,8 @@ return (
 this.props.work?this.props.work.map(post=>{
 return	(
 
-<Card key={post.id} 
+<div key={post.id}>
+<Card  
 style={cardStylez}
 >
 <CardBody>
@@ -70,7 +71,8 @@ style={this.actionStyle(post.action)}
 </CardLink>
 </CardBody>
 </Card>
-
+<hr />
+</div>
 )
 }):(	
 <div>
